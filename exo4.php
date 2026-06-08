a8ab70ae73ab24dcc5cb488d4f56c8f245bf7a11
Rokhaya Diallo NDIAYE <r45373136@gmail.com>
2026-06-08 15:50:07 +0000

    modification

--- exo4.php
+++ exo4.php
@@ -1,8 +1,26 @@
 <?php
-$couleur=["Rouge", "Vert", "Bleu", "Jaune",];
-if(isset($couleur[0])){
-    echo "la couleur est pésente";
+$couleurs=["Rouge", "Vert", "Bleu", "Jaune",];
+// if(isset($couleur[0])){
+//     echo "la couleur est pésente";
+// }else{
+//     echo "la couleur n'est pas pésente";
+// }
+
+$blueExiste=false;
+$noirExiste=false;
+foreach($couleurs as $couleur){
+    if($couleur=="Bleu"){
+        $blueExiste=true;
+    }
+
+     if($couleur=="Noir"){
+        $noirExiste=true;
+    }
+};
+if($blueExiste){
+    echo"la couleur est dans le tableau";
 }else{
-    echo "la couleur n'est pas pésente";
+     echo"la couleur n'est pas dans le tableau";
+   
 }
 ?>
