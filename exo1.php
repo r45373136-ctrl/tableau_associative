e47717f248629e47c211229d09435c4633f1e897
Rokhaya Diallo NDIAYE <r45373136@gmail.com>
2026-06-08 14:51:59 +0000

    modification exo 1

--- exo1.php
+++ exo1.php
@@ -3,8 +3,8 @@ $jours=["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",
 // print_r($jours);
 echo "le premier jour" . ": " . $jours[0]."<br>";
 echo  "le dernier jour" . ": " .$jours[6]."<br>";
-foreach($jours as $jours){
-    print_r($jours . "<br>");
+foreach($jours as $jour){
+    print_r($jour . "<br>");
 }
 
 ?>
