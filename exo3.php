b597b888ce213ff37dc146243a398bd5f773b142
Rokhaya Diallo NDIAYE <r45373136@gmail.com>
2026-06-08 15:22:11 +0000

    modification

--- exo3.php
+++ exo3.php
@@ -1,10 +1,22 @@
 <?php
 $notes=[12,15,14,10,18,];
-$somme= array_sum($notes);
+$somme=0;
+$max=$notes[0];
+// $somme= array_sum($notes);
+// echo "La somme est de ".$somme ."<br>";
+// $moyenne= $somme / count($notes);
+// echo "La moyenne est de ".$moyenne ."<br>";
+// $max= max($notes);
+// echo "La plus grande note est  ".$max;
+foreach($notes as $note){
+    $somme= $somme+$note;
+    if($note>$max){
+        $max=$note;
+    }
+}
+$moyenne = $somme/count($notes);
 echo "La somme est de ".$somme ."<br>";
-$moyenne= $somme / count($notes);
 echo "La moyenne est de ".$moyenne ."<br>";
-$max= max($notes);
 echo "La plus grande note est  ".$max;
 
 ?>
